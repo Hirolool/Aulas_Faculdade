c37579d2a79f3130904b44ef485df0f6778ef96b
Giovanni Paulo <118351700+Hirolool@users.noreply.github.com>
2025-05-06 21:15:19 -0300

    Add files via upload

---
+++ Aula 0605 - Programação.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int resultado;
+
+for (int i = 0; i < 10; i++){
+	 resultado = i;
+	  printf("Valor de i é = %d\n", resultado);
+	  i++;
+}
+
+//  for(int i = 1; i<=10; i=i + 1){
+//	 resultado = pow(i,3);
+//	  printf("Valor de i é = %d\n", resultado);
+
+int n = 0;
+
+while(n <100){
+	printf("Valor de n é = %d\n", n);
+	n++;
+}
+
+// int m = 11;
+// do {
+// printf("Valor de m é = %d\n", m);
+// m++;
+// }while(m < 10);
+// return 0;
