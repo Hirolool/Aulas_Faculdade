f487b807d0838c3ee89ad2b3bf1c902e56f99181
Giovanni Paulo <118351700+Hirolool@users.noreply.github.com>
2025-06-01 20:13:35 -0300

    Add files via upload

--- Revisão 2005.c
+++ Revisão 2005.c
@@ -1,39 +1,28 @@
-int a = 2;
-float b = 3;
-float c = 4;
-float resultado;
-resultado = b*c/a;
-printf("resultado: %2f\n", resultado); // 6
-resultado = (b*c)/a;
-printf("resultado: %2f\n", resultado); // 6
-resultado = b+b/a;
-printf("resultado: %2f\n", resultado); //4,5
+#include <stdio.h>
+#include <stdlib.h>
 
-for(int i = 1; i < 4; i++){
-    for(int j = 1; j < 4; j++){
-        printf(" (%02d, %02d)", i,j);
-    }
-    printf("\n");
+int resultado;
+
+for (int i = 0; i < 10; i++){
+	 resultado = i;
+	  printf("Valor de i é = %d\n", resultado);
+	  i++;
 }
-// (1,1) - (1,2) - (1,3)
-// (2,1) - (2,2) - (2,3)
-// (3,1) - (3,2) - (3,3)
 
-//Se i>j = (1,2) - (1,3) - (1,4) - (2,3) - (2,4) - (3,4) e o resto dos números é *.
+//  for(int i = 1; i<=10; i=i + 1){
+//	 resultado = pow(i,3);
+//	  printf("Valor de i é = %d\n", resultado);
 
-int x = 0;
-do{
-    printf("valor de X: %d", x);
-    x = x + 2;
-    printf("\n");
-}while(x < 6);
-// 0, 2, 4.
+int n = 0;
 
-float nota = 6;
-float presenca = 60;
-if (nota >=6 || presenca >=75){
-    printf("ALUNO APROVADO");
-}else{
-    printf("ALUNO REPROVADO");
+while(n <100){
+	printf("Valor de n é = %d\n", n);
+	n++;
 }
-// "||" significa OU, ou seja o Aluno é aprovado já que uma é Verdadeira e a outra Falsa.
+
+// int m = 11;
+// do {
+// printf("Valor de m é = %d\n", m);
+// m++;
+// }while(m < 10);
+// return 0;
